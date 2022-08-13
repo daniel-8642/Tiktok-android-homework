c7fbc815a718279f12d742c456dcd12517843216
王海涛 <859926750@qq.com>
2022-08-13 13:37:35 +0800

    viewpager2与tablelayout添加

---
+++ app/src/main/java/com/qxy/mzbzy/ui/page/moviesrank/MoviesrankInternetFragment.java
@@ -0,0 +1,73 @@
+package com.qxy.mzbzy.ui.page.moviesrank;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ObservableField;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModel;
+
+import com.qxy.mzbzy.R;
+import com.qxy.mzbzy.databinding.FragmentMoviesrankInternetBinding;
+import com.qxy.mzbzy.ui.App;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class MoviesrankInternetFragment extends Fragment {
+private MoviesrankInternetViewModel vm;
+private FragmentMoviesrankInternetBinding binding;
+
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        vm = App.getApp().getApplicationScopeViewModel(MoviesrankInternetFragment.MoviesrankInternetViewModel.class);
+
+        // 获取本页面databinding对象
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_moviesrank_internet,container,false);
+        // 设定ViewModel监听生命周期
+        binding.setLifecycleOwner(this);
+        // 为对象赋值
+        binding.setVm(vm);
+        binding.setClick(new MoviesrankInternetFragment.ClickProxy());
+        // 返回根view
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+        vm = null;
+    }
+    public class ClickProxy {
+        public void test() {
+            Toast.makeText(getContext(),"测试文本",Toast.LENGTH_SHORT).show();
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                String date = sdf.format(new Date());
+                vm.mText.set(date);
+            }
+        }
+    }
+
+    public static class MoviesrankInternetViewModel extends ViewModel {
+        //LiveData与Observable均可，LiveData在重新监听时回
+        // public final MutableLiveData<String> mText;
+        public final ObservableField<String> mText;
+
+        // 数据初始化
+        public MoviesrankInternetViewModel() {
+            mText = new ObservableField<>();
+            mText.set("This is home fragment");
+        }
+
+    }
+}
