9111a1239f95fd9050d69750114738e7f261ef17
王海涛 <859926750@qq.com>
2022-08-21 23:58:56 +0800

    取消顶部bar

--- app/src/main/java/com/qxy/mzbzy/ui/MainActivity.java
+++ app/src/main/java/com/qxy/mzbzy/ui/MainActivity.java
@@ -30,13 +30,8 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
-//    @Override
-//    public void setContentView(View view) {
-//        super.setContentView(view);
-//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
-//    }
 }
